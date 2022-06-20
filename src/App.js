fc8b4dca605d0eddb4ed3ce70406b8039e14b179
Engamba <engambasingha@gmail.com>
2022-06-20 16:36:48 +0530

    deleted fake products

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import "./App.css";
 import TodoApp from "./pages/todoapp";
 import Counter from "./pages/counter";
-import FakeProducts from "./pages/products";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <header className="App-head">
         <TodoApp />
         <Counter />
-        <FakeProducts />
       </header>
     </div>
   );
